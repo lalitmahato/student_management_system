ff08226290ca210ec4f1f8b9ceb2781ce6cc4551
Lalit Mahato <lalitmahato11@gmail.com>
2025-09-01 23:05:38 +0545

    Pylint configuration

--- user/decorators.py
+++ user/decorators.py
@@ -4,19 +4,18 @@ def unauthenticated_user(view_func):
     def wrapper_func(request, *args, **kwargs):
         if request.user.is_authenticated:
             return redirect('user:user_dashboard')
-        else:
-            return view_func(request, *args, **kwargs)
+        return view_func(request, *args, **kwargs)
     return wrapper_func
 
-def allowed_users(allowed_roles=[]):
+def allowed_users(allowed_roles=None):
     def decorator(view_func):
         def wrapper_func(request, *args, **kwargs):
             group = None
             if request.user.groups.exists():
                 group = request.user.groups.all()[0].name
-            if group in allowed_roles:
-                return view_func(request, *args, **kwargs)
-            else:
-                return redirect('user:error_401')
+            if allowed_roles:
+                if group in allowed_roles:
+                    return view_func(request, *args, **kwargs)
+            return redirect('user:error_401')
         return wrapper_func
     return decorator
